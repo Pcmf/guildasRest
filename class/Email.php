91e4fef3e15a6605cec3672eb8a82cbd32b711fd
Pcmf <pedroferreira2005@gmail.com>
2020-01-30 18:01:24 +0000

    send email

--- class/Email.php
+++ class/Email.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'db/DB.php';
+require_once 'sendEmail.php';
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -30,19 +31,32 @@ class Email {
 
     public function sendEmail($to, $obj) {
         //chamar as funções para enviar email
-        //TODO
-        $erro = "Não implementado";
+        if($to == 99) {
+            $list = $this->db->query("SELECT id, name, email, guilda FROM players WHERE ative=1 AND email<>'' ");
+        } else {
+           $list = $this->db->query("SELECT id, name, email, guilda FROM players WHERE ative=1 AND email IS NOT NULL AND guilda=:guilda ", [':guilda'=>$to]);
+        }
+        $email = new sendEmail();
+        $enviados_para ='';
+        foreach ($list as $ln) {
+            $obj->emailTo = $ln->email;
+            $obj->name = $ln->name;
+            $obj->guilda = $ln->guilda;
+            $erro = $email->send($obj);
+            $enviados_para .= $obj->emailTo."; erro: ".$erro."; "; 
+        }
+     //   $erro = "Não implementado";
         //Registar na BD
-        
-        return $this->insert($obj, $erro);
+        return $enviados_para;
+        return $this->insert($obj, $enviados_para);
       
     }
 
-    private function insert($obj, $erro) {
+    private function insert($obj, $report) {
         try {
-            $this->db->queryInsert("INSERT INTO emails(assunto, guilda, corpoemail, erro) "
-                    . " VALUES(:assunto, :guilda, :corpoemail, :erro)",
-                    [':assunto' => $obj->assunto, ':guilda' => $obj->guilda, ':corpoemail' => $obj->corpoemail, ':erro' => $erro]);
+            $this->db->queryInsert("INSERT INTO emails(assunto, guilda, corpoemail, report) "
+                    . " VALUES(:assunto, :guilda, :corpoemail, :report)",
+                    [':assunto' => $obj->assunto, ':guilda' => $obj->guilda, ':corpoemail' => $obj->corpoemail, ':report' => $report]);
             return $this->db->lastInsertId();
             
         } catch (Exception $exc) {
---
+++ class/sendEmail.php
@@ -0,0 +1,59 @@
+<?php
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+// Carregar as configurações do email
+require_once 'db/emailConfigs.php';
+
+class sendEmail {
+
+    private $mail;
+
+    public function __construct() {
+        // Instantiation and passing `true` enables exceptions
+        $this->mail = new PHPMailer(true);
+
+    }
+    
+    public function send($dt) {
+        echo json_encode($dt);
+        try {
+            //Server settings
+            $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $this->mail->isSMTP();                                            // Send using SMTP
+            $this->mail->Host = HOST;                    // Set the SMTP server to send through
+            $this->mail->SMTPAuth = true;                                   // Enable SMTP authentication
+            $this->mail->Username = 'teste@gestlifes.com';                     // SMTP username
+            $this->mail->Password = PASSWORD;                               // SMTP password
+            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $this->mail->Port = 587;                                    // TCP port to connect to
+            //Recipients
+            $this->mail->setFrom('teste@gestlifes.com', 'Testes');
+            //  $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+            $this->mail->addAddress($dt->emailTo);               // Name is optional
+            //  $mail->addReplyTo('info@example.com', 'Information');
+            //  $mail->addCC('cc@example.com');
+            //  $mail->addBCC('bcc@example.com');
+            // Attachments
+            //   $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            //   $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+            // Content
+            $this->mail->isHTML(true);                                  // Set email format to HTML
+            $this->mail->Subject = $dt->assunto;
+            $this->mail->Body = $dt->corpoemail;
+            $this->mail->AltBody = $dt->corpoemail;
+
+            $this->mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$this->mail->ErrorInfo}";
+        }
+    }
+}
+    
