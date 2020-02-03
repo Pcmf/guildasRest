802988f999cbe97270099b97231457f7128b34a5
Pcmf <pedroferreira2005@gmail.com>
2020-02-03 23:11:50 +0000

    delete

--- index.php
+++ index.php
@@ -1,92 +1,88 @@
 <?php
+
 require_once 'class/Player.php';
 require_once 'class/User.php';
 require_once 'class/Guilda.php';
 require_once 'class/Email.php';
 
-
 function checkToken($token) {
     $obj = new User();
     return 1;
-   // return $obj->checkToken($token)->result;
+    // return $obj->checkToken($token)->result;
 }
-
 
 //$headers = apache_request_headers();
 //if ($_GET['url'] != "auth" && checkToken($headers['token']) == 0) {
 //    http_response_code(401);
 //} else {
-
-
 //POSTS
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        $postBody = file_get_contents("php://input");
-        $postBody = json_decode($postBody);
-        //LOG IN
-        if ($_GET['url'] == "auth") {
-            $user = new User();
-            echo json_encode($user->checkUser($postBody->username, $postBody->password));
-            http_response_code(200);
-            
-        } elseif ($_GET['url']=='players') {
-            $ob = new Player();
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $postBody = file_get_contents("php://input");
+    $postBody = json_decode($postBody);
+    //LOG IN
+    if ($_GET['url'] == "auth") {
+        $user = new User();
+        echo json_encode($user->checkUser($postBody->username, $postBody->password));
+        http_response_code(200);
+    } elseif ($_GET['url'] == 'players') {
+        $ob = new Player();
+        echo json_encode($ob->insert($postBody));
+        http_response_code(200);
+    } else {
+        http_response_code(405);
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] == "PUT") {
+    $postBody = file_get_contents("php://input");
+    $postBody = json_decode($postBody);
+
+    if ($_GET['url'] == 'players') {
+        $ob = new Player();
+        if ($_GET['id'] == 0) {
             echo json_encode($ob->insert($postBody));
-            http_response_code(200);
-            
         } else {
-            http_response_code(405);
+            echo json_encode($ob->update($_GET['id'], $postBody));
         }
-        
-    } elseif ($_SERVER['REQUEST_METHOD'] == "PUT") {
-        $postBody = file_get_contents("php://input");
-        $postBody = json_decode($postBody);
-        
-        if($_GET['url']=='players'){
-            $ob = new Player();
-            if($_GET['id']==0){
-               echo json_encode($ob->insert($postBody)); 
-            }else {
-                echo json_encode($ob->update($_GET['id'], $postBody));
-            }
-            http_response_code(200);
-            
-        } elseif ($_GET['url']=='emails') {
-            $ob = new Email();
-            echo json_encode($ob->sendEmail($_GET['id'], $postBody));
-            http_response_code(200);
-            
-        }else {
-             http_response_code(405);
-        }
-    } elseif ($_SERVER['REQUEST_METHOD'] == "GET") {
-        
-        if($_GET['url']=='players'){
-            $ob = new Player();
-            if(isset($_GET['id'])){
-                echo json_encode($ob->getById($_GET['id']));
-            } else {
-                echo json_encode($ob->getAll());
-            }
-            http_response_code(200);
-            
-        } elseif ($_GET['url']=='guildas') {
-            $ob = new Guilda();
+        http_response_code(200);
+    } elseif ($_GET['url'] == 'emails') {
+        $ob = new Email();
+        echo json_encode($ob->sendEmail($_GET['id'], $postBody));
+        http_response_code(200);
+    } else {
+        http_response_code(405);
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] == "GET") {
+
+    if ($_GET['url'] == 'players') {
+        $ob = new Player();
+        if (isset($_GET['id'])) {
+            echo json_encode($ob->getById($_GET['id']));
+        } else {
             echo json_encode($ob->getAll());
-            http_response_code(200);
-            
-        } elseif ($_GET['url']=='emails') {
-            $ob = new Email();
-            if(isset($_GET['id'])){
-                echo json_encode($ob->getById($_GET['id']));
-            } else {
-                echo json_encode($ob->getAll());
-            }
-            http_response_code(200);
-            
-        } else {
-            http_response_code(405);
         }
-    
+        http_response_code(200);
+    } elseif ($_GET['url'] == 'guildas') {
+        $ob = new Guilda();
+        echo json_encode($ob->getAll());
+        http_response_code(200);
+    } elseif ($_GET['url'] == 'emails') {
+        $ob = new Email();
+        if (isset($_GET['id'])) {
+            echo json_encode($ob->getById($_GET['id']));
+        } else {
+            echo json_encode($ob->getAll());
+        }
+        http_response_code(200);
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] == "DELETE") {
+
+    if ($_GET['url'] == 'players') {
+        $ob = new Player();
+        echo json_encode($ob->delete($_GET['id']));
+        http_response_code(200);
+    }
+} else {
+    http_response_code(405);
 }
     
-//}
+
+    
